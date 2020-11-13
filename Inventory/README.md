c5ab6b79fde45e84fe233f291add30c2cb7e3e71
Michael <lef17004@byui.edu>
2020-11-13 16:02:48 -0700

    First Commit

---
+++ Inventory/README.md
@@ -0,0 +1,17 @@
+#  Objective-C Class Test
+
+## Overview
+I have never programmed in Objective-C before, so I made this program to learn how classes work in this language. I have a simple class called inventory I created to experiment with. 
+
+## Development Envirnment
+I created this using Xcode version 12.1. To set up this project, first install Xcode if you don't have it already. Then go to Source Control->Clone and then paste in the link to this Github repository. The project should download and be setup automatically. 
+
+![Screenshot showing the output of the program.](terminal.png) 
+
+## Useful Websites
+[Techotopia](https://www.techotopia.com/index.php/Objective-C_Inheritance)
+[C for Dummies](https://www.dummies.com/programming/c/how-to-generate-random-numbers-in-c-programming/)
+[Journal Dev](https://www.journaldev.com/10182/nsarray-nsmutablearray-objective-c-array)
+[Tutorialspoint](https://www.tutorialspoint.com/objective_c/index.htm)
+[Apple's Ojective-C Book](https://developer.apple.com/library/archive/documentation/Cocoa/Conceptual/ProgrammingWithObjectiveC/Introduction/Introduction.html)
+[Python Reference](https://docs.python.org/3/tutorial/index.html)
--- Inventory/main.m
+++ Inventory/main.m
@@ -6,11 +6,39 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "Inventory.h"
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        printf("Testing the defualt constructor.\n");
+        Inventory * myStore = [[Inventory alloc]init];
+        [myStore showInventory];
+        [myStore setCandy:120];
+        
+        printf("Testing the setters.\n");
+        [myStore setChips:40];
+        [myStore setDrinks:33];
+        [myStore setCookies:200000];
+        [myStore setBrownies:66];
+        [myStore showInventory];
+        
+        
+        printf("Testing the nondefault constructor.\n");
+        Inventory *mySecondStore = [[Inventory alloc]initWithCandy:90 andCookies:20 andBrownies:30 andDrinks:40 andChips:22];
+        [mySecondStore showInventory];
+        
+        
+        printf("Random Experiments\n\n");
+        printf("Setting one inventory object equal to another.\n");
+        //They are pointers, so I suspect it will just change which object the pointer is pointing at.
+        myStore = mySecondStore;
+        [myStore showInventory];
+        
+        printf("Checking to see if changing mySecondStore will also change myStore.\n");
+        [mySecondStore setCandy:-30];
+        [myStore showInventory];
+        
+        
     }
     return 0;
 }
